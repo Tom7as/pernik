91c21546df499edb48a2f83224f21b6519593904
Tom7as <tom7as@seznam.cz>
2017-09-24 23:02:00 +0200

    zakladni 4 lepitka

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -16,27 +17,31 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class Main extends Application {
-
+    TextArea textArea = new TextArea();
     @Override
     public void start(Stage primaryStage) throws Exception{
         TextField textField = new TextField("http://stackoverflow.com/questions/39898704");
 
-        Button liepitkaBileButton = new Button("Bílé");
-        Button liepitkaBarevneButton = new Button("Barevné");
-        Button liepitkaPrirodniButton = new Button("Přírodní barvy");
-        Button liepitkaBezPolevyButton = new Button("Bez polevy");
+        Button lepitkaBileButton = new Button("Bílé");
+        Button lepitkaBarevneButton = new Button("Barevné");
+        Button lepitkaPrirodniButton = new Button("Přírodní barvy");
+        Button lepitkaBezPolevyButton = new Button("Bez polevy");
 
 
-//        EventHandler<ActionEvent> handler = e -> open("Bíle.doc");
-//        liepitkaBileButton.setOnAction(handler);
-
-        liepitkaBileButton.setOnAction(e -> open("Bile.doc"));
+        lepitkaBileButton.setOnAction(e -> open(".\\lepitka\\Bile.doc"));
+        lepitkaBarevneButton.setOnAction(d -> open(".\\lepitka\\Barevne.doc"));
+        lepitkaPrirodniButton.setOnAction(d -> open(".\\lepitka\\Barevne_prirodni.doc"));
+        lepitkaBezPolevyButton.setOnAction(d -> open(".\\lepitka\\bez_polevy.doc"));
 
         VBox root = new VBox(5,
                 new HBox(new Label("Lepítka")),
-                new HBox(liepitkaBileButton)
+                new HBox(lepitkaBileButton, lepitkaBarevneButton, lepitkaPrirodniButton, lepitkaBezPolevyButton)
         );
 
         root.setPadding(new Insets(20));
@@ -53,5 +58,4 @@ public class Main extends Application {
     private void open(String resource) {
         getHostServices().showDocument(resource);
     }
-
 }
